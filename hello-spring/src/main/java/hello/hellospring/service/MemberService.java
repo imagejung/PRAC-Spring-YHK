8ff3777f1c6f953f051966ad071bb405e84cfeb8
imagejung <ysjung9312@gmail.com>
2024-01-17 23:34:13 +0900

    스프링 입문, 스프링 빈과 의존관계(컴포넌트 스캔과 자동 의존관계 설정)

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,17 +3,20 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
-
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     // 테스트를 같은 repository로 하기 위해 아래와 같이 변경
     private final MemberRepository memberRepository;
 
+    @Autowired
     // DI 의존성 주입 관련
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
